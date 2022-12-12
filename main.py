a21227ce710e4176f157c65e5a83759454881bd3
yujaegwon <dbwornjs12345@naver.com>
2022-12-12 09:33:13 +0900

    태그 추가

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ now = datetime.datetime.utcnow()
 
 # Insert some data into table
 sqlQuery = "INSERT INTO dsdl_item (dsdl_grp_cd, dsdl_item_cd, dsdl_item_nm, acvt_stts, otpt_sqnc, reg_user_id, reg_dtm) VALUES (%s, %s, %s, %s, %s, %s, %s)"
-params = ('A0001', 'A0003', '맞팔', 'Y', 0, 'SYSTEM', now.strftime('%Y%m%d%H%M%S'))
+params = ('A0001', 'A0004', '데일리', 'Y', 0, 'SYSTEM', now.strftime('%Y%m%d%H%M%S'))
 cursor.execute(sqlQuery, params)
 print("Inserted",cursor.rowcount,"row(s) of data.")
 
