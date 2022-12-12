2ad0aedd2800c2aed9e505170a17fe6100ffe420
yujaegwon <dbwornjs12345@naver.com>
2022-12-12 10:59:54 +0900

    db tester

--- dbTester.py
+++ dbTester.py
@@ -8,22 +8,22 @@ print("========= 구분자 DB Tester Start =========")
 print("1. 등록")
 params = ('A0001', 'A0005', '일상', 'Y', 0,
           'SYSTEM', now.strftime('%Y%m%d%H%M%S'))
-rowcount = 등록_구분자항목(params)
+rowcount = db.queries.등록_구분자항목(params)
 print("Inserted", rowcount, "row(s) of data.")
 
 print("2. 조회")
 params = ('A0001')
-rows = 조회_구분자항목(params)
+rows = db.queries.조회_구분자항목(params)
 for row in rows:
     print("Data row = (%s)" % (row[0]))
 
 print("3. 수정")
 params = ('좋아요반사', 'A0001', 'A0005')
-rowcount = 수정_구분자항목(params)
+rowcount = db.queries.수정_구분자항목(params)
 print("Updated", rowcount, "row(s) of data.")
 
 print("4. 삭제")
 params = {'dsdl_grp_cd': "A0001", 'dsdl_item_cd': 'A0005'}
-rowcount = 삭제_구분자항목(params)
+rowcount = db.queries.삭제_구분자항목(params)
 print("Deleted", rowcount, "row(s) of data.")
 print("========= 구분자 DB Tester End =========")
